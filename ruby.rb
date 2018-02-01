ee500fa8ec55e64353b6317d86b475fd0b381bdd
Mark LeBlanc <mark@marksleblanc.com>
2018-02-01 02:32:26 -0500

    second commit

---
+++ ruby.rb
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<title>Sunbolt - Home</title>
+		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Fira+Sans">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+		<section id="intro">
+			<header id="header" class="container clearfix">
+				<a href="/" class="float-left logo"><img src="images/sunbolt-logo.png" width="220" height="30"></a>
+				<nav id="mainnav" class="navbar navbar-toggleable-md navbar-light float-right mr-auto">
+					<button class="custom-toggler dropdown-toggle" type="button" data-toggle="collapse" data-target="#mainnavdropdown" aria-controls="mainnavdropdown" aria-expanded="false">
+				    	<span class="navbar-toggler-icon"></span>
+				  	</button>
+				  	<div class="collapse navbar-collapse" id="mainnavdropdown">
+					    <ul class="navbar-nav">
+						    <li class="nav-item active">
+						        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+						    </li>
+						    <li class="nav-item">
+						        <a class="nav-link" href="#">Inner Page</a>
+						    </li>
+						    <li class="nav-item">
+						        <a class="nav-link" href="#">Inner Page</a>
+						    </li>
+						    <li class="nav-item">
+						        <a class="nav-link" href="#">Inner Page</a>
+						    </li>
+						</ul>
+					</div>
+				</nav>
+			</header>
+		</section>
+		<script type="text/javascript" src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+		<script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	</body>
+</html>
